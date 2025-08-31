a94ed41965673fe0c0dd280ed7c30e13832d3a9c
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-08-31 14:27:20 +0800

    perf: add iterator

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,16 @@ pub struct Config {
     pub ignore_case: bool,
 }
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Error, no enough arguments, should use: \nminigrep pattern file_path");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        // feat: add env for case sensitive
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config {
             query,
@@ -31,7 +33,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     } else {
         search(&config.query, &contents)
     };
-    
+
     for line in results {
         println!("{line}");
     }
@@ -40,25 +42,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 // public core function: search for minigrep
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result: Vec<&str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    result
+    contents
+        .lines()
+        .filter(|line| line.contains(&query))
+        .collect()
 }
 
 // public core function: search while case sensitive
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 // test module
